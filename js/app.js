edc3d7f4d342c85a25551510c9f72023493d316a
Fofiko <foteinid@gmail>
2018-09-10 08:07:30 +0100

    Add helper functions

--- js/app.js
+++ js/app.js
@@ -1,11 +1,14 @@
+const select = element => document.querySelector(element);
+const create = element => document.createElement(element);
+
 document.addEventListener('DOMContentLoaded', () => {
 
   // console.log('Yass');
 
-  const form = document.querySelector('#form');
+  const form = select('#form');
   form.addEventListener('submit', handleFormSubmit);
 
-  const deleteAllButton = document.querySelector('#delete-all');
+  const deleteAllButton = select('#delete-all');
   deleteAllButton.addEventListener('click', handleDeleteAllClick);
 });
 
@@ -13,32 +16,32 @@ const handleFormSubmit = function (event) {
   event.preventDefault();
 
   const olympiansListItem = createOlympiansListItem(event.target);
-  const olympiansList = document.querySelector('#olympians-list');
+  const olympiansList = select('#olympians-list');
   olympiansList.appendChild(olympiansListItem);
 
   event.target.reset();
 }
 
 const createOlympiansListItem = function (form) {
-  const olympiansListItem = document.createElement('li');
+  const olympiansListItem = create('li');
   olympiansListItem.classList.add('olympians-list-item');
 
-  const greek_name = document.createElement('h3');
+  const greek_name = create('h3');
   greek_name.textContent = form.greek_name.value;
   olympiansListItem.appendChild(greek_name);
 
-  const latin_name = document.createElement('h3');
+  const latin_name = create('h3');
   latin_name.textContent = form.latin_name.value;
   olympiansListItem.appendChild(latin_name);
 
-  const dominion = document.createElement('p');
+  const dominion = create('p');
   dominion.textContent = form.dominion.value;
   olympiansListItem.appendChild(dominion);
 
-    return olympiansListItem;
+  return olympiansListItem;
 }
 
-  const handleDeleteAllClick = function (event) {
-    const olympiansList = document.querySelector('#olympians-list');
-    olympiansList.innerHTML = '';
-  }
+const handleDeleteAllClick = function (event) {
+  const olympiansList = select('#olympians-list');
+  olympiansList.innerHTML = '';
+}
